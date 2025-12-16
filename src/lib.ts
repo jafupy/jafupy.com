3dd5b101d937ba3de9de621fa53182b6e68f6b61
Jacob <104758482+JaFu-py@users.noreply.github.com>
2025-12-16 17:58:54 +0000

    Delete deprecated lib folder and utilities files

--- src/lib.ts
+++
@@ -1,8 +0,0 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
-
-export function cn(...inputs: ClassValue[]): string {
-  return twMerge(clsx(inputs));
-}
-export const logo =
-  "https://utfs.io/f/d9850d67-72a8-49ad-898e-0fb355fd9671-1zbfv.svg";
